dd92dec8e4e83e08717964ada6fa9a8697ac1e4b
raulanatol <raul@natol.es>
2018-08-31 19:33:31 +0100

    Cut index.html to parts

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,45 @@
 $.noConflict();
 
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 
-	"use strict";
+  "use strict";
 
-	[].slice.call( document.querySelectorAll( 'select.cs-select' ) ).forEach( function(el) {
-		new SelectFx(el);
-	} );
+  [].slice.call(document.querySelectorAll('select.cs-select')).forEach(function (el) {
+    new SelectFx(el);
+  });
 
-	jQuery('.selectpicker').selectpicker;
-
-
-	$('#menuToggle').on('click', function(event) {
-		$('body').toggleClass('open');
-	});
-
-	$('.search-trigger').on('click', function(event) {
-		event.preventDefault();
-		event.stopPropagation();
-		$('.search-trigger').parent('.header-left').addClass('open');
-	});
-
-	$('.search-close').on('click', function(event) {
-		event.preventDefault();
-		event.stopPropagation();
-		$('.search-trigger').parent('.header-left').removeClass('open');
-	});
-
-	// $('.user-area> a').on('click', function(event) {
-	// 	event.preventDefault();
-	// 	event.stopPropagation();
-	// 	$('.user-menu').parent().removeClass('open');
-	// 	$('.user-menu').parent().toggleClass('open');
-	// });
+  jQuery('.selectpicker').selectpicker;
 
 
+  $('#menuToggle').on('click', function (event) {
+    $('body').toggleClass('open');
+  });
+
+  $('.search-trigger').on('click', function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    $('.search-trigger').parent('.header-left').addClass('open');
+  });
+
+  $('.search-close').on('click', function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    $('.search-trigger').parent('.header-left').removeClass('open');
+  });
+
+  // $('.user-area> a').on('click', function(event) {
+  // 	event.preventDefault();
+  // 	event.stopPropagation();
+  // 	$('.user-menu').parent().removeClass('open');
+  // 	$('.user-menu').parent().toggleClass('open');
+  // });
+
+  loadContent();
+
+
+  function loadContent() {
+    $('#left-panel').load('content/navbar.html');
+    $('#header').load('content/header.html');
+    $('#main-container').load('content/home.html');
+  }
 });
